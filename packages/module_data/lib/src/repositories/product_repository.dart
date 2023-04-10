474d603d1340616bcf5877373d5fd5c4867cee97
Garri <3434455@mail.ru>
2023-04-10 19:48:52 +0500

    Refactoring

--- packages/module_data/lib/src/repositories/product_repository.dart
+++ packages/module_data/lib/src/repositories/product_repository.dart
@@ -1,50 +1,51 @@
 import '../models/product.dart';
 import '../models/cart.dart';
 
-List<Product> _products = const [
-  Product(0, 'First product title'),
-  Product(1, 'Second product title'),
-  Product(2, 'Third product title'),
-  Product(3, 'Fourth product title'),
-  Product(4, 'Fifth product title'),
-  Product(5, 'Sixth product title'),
-  Product(6, 'Seventh product title'),
-  Product(7, 'Eighth product title'),
-  Product(8, 'Ninth product title'),
-  Product(9, 'Tenth product title'),
-  Product(10, 'Eleventh product title'),
+List<ProductData> _products = const [
+  ProductData(id: 0, title: 'First product title'),
+  ProductData(id: 1, title: 'Second product title'),
+  ProductData(id: 2, title: 'Third product title'),
+  ProductData(id: 3, title: 'Fourth product title'),
+  ProductData(id: 4, title: 'Fifth product title'),
+  ProductData(id: 5, title: 'Sixth product title'),
+  ProductData(id: 6, title: 'Seventh product title'),
+  ProductData(id: 7, title: 'Eighth product title'),
+  ProductData(id: 8, title: 'Ninth product title'),
+  ProductData(id: 9, title: 'Tenth product title'),
+  ProductData(id: 10, title: 'Eleventh product title'),
 ];
 
 abstract class ProductRepositoryInterface {
-  Future<List<Product>> fetchAll();
-  Future<Product> fetchOne(int productId);
-  Future<CartData> addProductToCart(Product product, CartData cart);
-  Future<CartData> removeProductFromCart(Product product, CartData cart);
+  Future<List<ProductData>> fetchAll();
+  Future<ProductData> fetchOne(int productId);
+  Future<CartData> addProductToCart(ProductData product, CartData cart);
+  Future<CartData> removeProductFromCart(ProductData product, CartData cart);
 }
 
 class ProductRepository implements ProductRepositoryInterface {
   @override
-  Future<CartData> addProductToCart(Product product, CartData cart) async {
+  Future<CartData> addProductToCart(ProductData product, CartData cart) async {
     await Future.delayed(const Duration(seconds: 1));
-    return CartData(cart.id, cart.title, [...cart.products, product]);
+    return cart.copyWith(products: [...cart.products, product]);
   }
 
   @override
-  Future<List<Product>> fetchAll() async {
+  Future<List<ProductData>> fetchAll() async {
     await Future.delayed(const Duration(seconds: 2));
     return _products;
   }
 
   @override
-  Future<Product> fetchOne(int productId) async {
+  Future<ProductData> fetchOne(int productId) async {
     await Future.delayed(const Duration(seconds: 2));
     return _products[productId];
   }
 
   @override
-  Future<CartData> removeProductFromCart(Product product, CartData cart) async {
+  Future<CartData> removeProductFromCart(ProductData product, CartData cart) async {
     await Future.delayed(const Duration(seconds: 1));
-    cart.products.remove(product);
-    return cart;
+    List<ProductData> newProducts = cart.products;
+    newProducts.remove(product);
+    return cart.copyWith(products: newProducts);
   }
 }
