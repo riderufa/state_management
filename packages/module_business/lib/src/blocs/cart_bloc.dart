f56cd57ab49664e622adad0e02b680e5d89e305e
Garri <3434455@mail.ru>
2023-04-17 20:10:12 +0500

    Remove mapEventToState

--- packages/module_business/lib/src/blocs/cart_bloc.dart
+++ packages/module_business/lib/src/blocs/cart_bloc.dart
@@ -1,38 +1,22 @@
-import 'dart:async';
-
 import 'package:module_business/src/events/cart_events.dart';
 import 'package:module_data/module_data.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
 class CartBloc extends Bloc<Event, AppState> {
   final ProductRepository _productRepository;
 
-  CartBloc(this._productRepository) : super(const AppState());
-
-  @override
-  Stream<AppState> mapEventToState(Event action) async* {
-    if (action is GetProductsEvent) {
-      try {
-        final products = await _productRepository.fetchAll();
-        yield state.copyWith(products: products);
-      } on Exception {
-        yield state.copyWith(products: []);
-      }
-    } else if (action is AddProductCartEvent) {
-      try {
-        final products = await _productRepository.addProductToCart(state.cartProducts, action.product);
-        yield state.copyWith(cartProducts: products);
-      } on Exception {
-        yield state.copyWith(products: state.products);
-      }
-    } else if (action is RemoveProductCartEvent) {
-      try {
-        final products = await _productRepository.removeProductFromCart(state.cartProducts, action.product);
-        yield state.copyWith(cartProducts: products);
-      } on Exception {
-        yield state.copyWith(products: state.products);
-      }
-    }
+  CartBloc(this._productRepository) : super(const AppState()) {
+    on<GetProductsEvent>((event, emit) async {
+      final products = await _productRepository.fetchAll();
+      return emit(state.copyWith(products: products));
+    });
+    on<AddProductCartEvent>((event, emit) async {
+      final products = await _productRepository.addProductToCart(state.cartProducts, event.product);
+      return emit(state.copyWith(cartProducts: products));
+    });
+    on<RemoveProductCartEvent>((event, emit) async {
+      final products = await _productRepository.removeProductFromCart(state.cartProducts, event.product);
+      return emit(state.copyWith(cartProducts: products));
+    });
   }
 }
